ad9b255beae43456f6ff09d446cedf9ad6fadac2
tom <mail>
2022-10-15 09:23:58 +0800

    添加random以及jest

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import {readFileSync} from 'fs';
-import {fileURLToPath} from 'url';
-import {dirname, resolve} from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { info } from 'console';
 
 const url = import.meta.url; // 获取当前脚本文件的url
 const path = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
-const data = readFileSync(path, {encoding: 'utf-8'});
-console.log(data);
+const data = readFileSync(path, { encoding: 'utf-8' });
+console.info('data', data)
+
