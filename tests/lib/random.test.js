c79d016710f1a067c1cc62bb97d915b9471cef4b
tom <mail>
2022-10-15 09:25:23 +0800

    补上数组类型转换

--- tests/lib/random.test.js
+++ tests/lib/random.test.js
@@ -3,10 +3,9 @@ import { randomPick, shuffle } from "../../lib/random";
 describe('randomPick', () => {
     for (let offset = 1; offset < 100; offset++) {
         it(`randomPick: ${offset}`, () => {
-            const oldArr = shuffle(new Array(offset).fill(1).map((_, index) => index + 1));
+            const oldArr = shuffle(new Array(offset).fill(1).map((_, index) => (index + 1).toString()));
             const arr = [...oldArr]
             const random = randomPick(arr);
-            console.info(random)
             expect(random).toEqual(oldArr[oldArr.length - 1]);
         });
     }
